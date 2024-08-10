df97386d8ea33c8c6e963cf0bae9f186303587de
Gaston <gonzalezgastonariel@gmail.com>
2024-08-09 22:28:22 -0300

    Se agrega fetch para obtener los datos de las canciones desde la API

--- src/components/Songs/CardSong/CardSong.tsx
+++ src/components/Songs/CardSong/CardSong.tsx
@@ -1,17 +1,20 @@
 import './CardSong.css';
 
-export function CardSong() {
+interface CardSongProps {
+    title: string;
+    artist: string;
+    album: string;
+    image: string;
+}
+
+export function CardSong({ title, artist, album, image }: CardSongProps) {
     return (
         <div className="CardSong">
-            <img 
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkT6fnreuWwFFn_Bk9w0h9OVAgyso_Zw0QXg&s" 
-                alt="Song album cover" 
-                className="CardSong-image" 
-            />
+            <img src={image} alt={`Album cover for ${title}`} className="CardSong-image" />
             <div className="CardSong-info">
-                <h2 className="CardSong-title">Song Title</h2>
-                <p className="CardSong-artist">Artist Name</p>
-                <p className="CardSong-album">Album Name</p>
+                <h2 className="CardSong-title">{title}</h2>
+                <p className="CardSong-artist">{artist}</p>
+                <p className="CardSong-album">{album}</p>
             </div>
         </div>
     );
--- src/components/Songs/Songs.tsx
+++ src/components/Songs/Songs.tsx
@@ -1,9 +1,49 @@
-import { CardSong } from "./CardSong/CardSong";
+import { useState, useEffect } from 'react';
+import { CardSong } from './CardSong/CardSong';
+import './CardSong/CardSong.css';
+import './Songs.css';
+
+interface Song {
+    title: string;
+    artist: string;
+    album: string;
+    cover: string;
+}
 
 export function Songs() {
+    const [songs, setSongs] = useState<Song[]>([]);
+
+    useEffect(() => {
+        fetchSongs();
+    }, []);
+
+    const fetchSongs = async () => {
+        try {
+            const response = await fetch('https://sandbox.academiadevelopers.com/harmonyhub/songs?page_size=181');
+            const data = await response.json();
+
+            
+            if (data && data.results) {
+                setSongs(data.results); 
+            } else {
+                console.error('Estructura de datos no esperada:', data);
+            }
+        } catch (error) {
+            console.error('Error fetching songs:', error);
+        }
+    };
+
     return (
-        <div>
-            <CardSong />
+        <div className="songs-container">
+            {songs.map((song, index) => (
+                <CardSong
+                    key={index}
+                    title={song.title}
+                    artist={song.artist}
+                    album={song.album}
+                    image={song.cover}
+                />
+            ))}
         </div>
     );
 }
